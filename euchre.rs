ce30c2fcefedf998050e4e3092b0027e64996f22
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2025-04-23 11:12:18 -0500

    Update and rename euchre.py to euchre.rs

---
+++ euchre.rs
@@ -0,0 +1,25 @@
+
+const let suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+const ranks = ["9", "10", "Jack", "Queen", "King"]
+
+enum Suits {
+  Hearts,
+  Clubs,
+  Diamonds,
+  Spades,
+}
+
+enum Ranks {
+  Nine,
+  Ten,
+  Jack, 
+  Queen,
+  King,
+}
+
+struct Card {
+  suit : String
+  ranks : 
+}
+
+      
