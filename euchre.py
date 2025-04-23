803ebaab6afdf3da5ed97cdffc8610a3c18d1226
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2025-04-23 08:14:24 -0500

    Update euchre.py

--- euchre.py
+++ euchre.py
@@ -1,1 +1,23 @@
+import random
+import time
 
+
+
+class deck:
+  def __init__(self):
+    self.shuffled = []
+    self.unshuffled = ["ah", "9h", "10h", "jh", "qh", "kh", "ad", "9d", "10d", "jd", "qd", "kd", "as", "9s", "10s", "js", "qs", "ks", "ac", "9c", "10c", "jc", "qc", "kc"]
+    
+  def shuffle(self):
+    if self.unshuffled == []:
+      self.unshuffled = self.shuffled
+      self.shuffled.clear()
+    for i in range(len(self.unshuffled)):
+      random_card = random.choice(self.unshuffled)
+      random_card_index = self.unshuffled.index(random_card)
+      self.unshuffled.pop(random_card_index)
+      self.shuffled.append(random_card)
+      
+
+deck = deck()
+print(deck.shuffled())
