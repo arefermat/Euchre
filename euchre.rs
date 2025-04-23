8c2973ea2de62fc3854eedc4db20d50d5642d5d3
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2025-04-23 14:35:56 -0500

    Update euchre.rs

--- euchre.rs
+++ euchre.rs
@@ -1,3 +1,6 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 
 enum Suits {
   Hearts,
@@ -16,16 +19,68 @@ enum Ranks {
 }
 
 struct Card {
-  suit : Suits,
-  ranks : Ranks,
-  trump : bool,
-  right_bower : bool,
-  left_bower : bool,
-  value : i32,
+  Suit : Suits,
+  Ranks : Ranks,
+  Trump : bool,
+  Right_Bower : bool,
+  Left_Bower : bool,
+  Value : i32,
 }
 
-fn build() -> [Card] {
-  let suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-  let ranks = ["Ace", "9", "Ten", "Jack", "Queen", "King"]
+struct Player {
+  ID : i32,
+  Cards : Vec<Card>,
+  Points : i32,
+  Teammate : i32,
+  Dealer : bool,
 }
-      
+
+fn build() -> Vec<Card> {
+  let suits = vec![Suits::Hearts, Suits::Diamonds, Suits::Spades, Suits::Clubs];
+  let ranks = vec![Ranks::Ace, Ranks::Nine, Ranks::Ten, Ranks::Jack, Ranks::Queen, Ranks::King];
+  let mut deck = Vec::new();
+
+  for suit in &suits {
+    for rank in &ranks {
+      let _ = Card {
+        Suit : &suit,
+        Ranks : &rank,
+        Trump : false,
+        Right_Bower : false,
+        Left_Bower : false,
+        Value : 0, 
+      };
+      deck.push(&card);
+    }
+  }
+  deck
+}
+
+fn shuffle(deck: Vec<Card>) -> Vec<Card> {
+  let unshuffled = deck;
+  
+  let mut shuffled = Vec::new();
+  let mut rng = thread_rng();
+
+  shuffled = &unshuffled.shuffle(&mut rng);
+  
+  shuffled
+}
+
+
+
+fn deal(deck: Vec<Card>) -> Player, Player, Player, Player, Vec<Card> {
+  for player in 0..4 {
+    let cards = Vec::new()
+    for card in 0..5 {
+      cards.push(deck.choice())
+    }
+    let _ = Player{
+      ID : player,
+      Cards : cards,
+      Points : 0,
+      Teammate : 2,
+      Dealer : true,
+    };
+  }
+}
